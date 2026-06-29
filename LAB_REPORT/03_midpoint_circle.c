2c46ae1d8142dd0ff97d8c0cbf6f3385155002e0
Dinesh Poudel <dineshp4297501@gmail.com>
2026-06-29 19:44:09 +0545

    LAB

---
+++ LAB_REPORT/03_midpoint_circle.c
@@ -0,0 +1,85 @@
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+#include <stdlib.h>
+
+#define WIDTH  800
+#define HEIGHT 600
+
+int cx = 400, cy = 300, radius = 150;
+
+void plotCirclePoints(int xc, int yc, int x, int y) {
+    glVertex2i(xc + x, yc + y);
+    glVertex2i(xc - x, yc + y);
+    glVertex2i(xc + x, yc - y);
+    glVertex2i(xc - x, yc - y);
+    glVertex2i(xc + y, yc + x);
+    glVertex2i(xc - y, yc + x);
+    glVertex2i(xc + y, yc - x);
+    glVertex2i(xc - y, yc - x);
+}
+
+void drawMidpointCircle(int xc, int yc, int r) {
+    int x = 0, y = r;
+    int p = 1 - r; /* initial decision parameter */
+
+    glColor3f(1.0f, 1.0f, 0.0f);
+    glPointSize(3.0f);
+    glBegin(GL_POINTS);
+
+    plotCirclePoints(xc, yc, x, y);
+    while (x < y) {
+        x++;
+        if (p < 0) {
+            p += 2 * x + 1;
+        } else {
+            y--;
+            p += 2 * x - 2 * y + 1;
+        }
+        plotCirclePoints(xc, yc, x, y);
+    }
+
+    glEnd();
+    glFlush();
+}
+
+void display(void) {
+    glClear(GL_COLOR_BUFFER_BIT);
+    drawMidpointCircle(cx, cy, radius);
+
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glPointSize(8.0f);
+    glBegin(GL_POINTS);
+        glVertex2i(cx, cy);
+    glEnd();
+
+    glFlush();
+}
+
+void keyboard(unsigned char key, int x, int y) {
+    if (key == 27) exit(0);
+}
+
+void init(void) {
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0, WIDTH, 0, HEIGHT);
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(WIDTH, HEIGHT);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Midpoint Circle Algorithm");
+
+    init();
+    glutDisplayFunc(display);
+    glutKeyboardFunc(keyboard);
+    glutMainLoop();
+
+    return 0;
+}
